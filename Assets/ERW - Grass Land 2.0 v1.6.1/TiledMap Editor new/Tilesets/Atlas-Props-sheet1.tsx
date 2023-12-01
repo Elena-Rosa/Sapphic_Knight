1e2db2179b6f7fb12b7c5004b42cda553bd18553
Jordan <mod.jordan@icloud.com>
2023-11-30 22:57:07 -0800

    TownMap Update.

--- Assets/ERW - Grass Land 2.0 v1.6.1/TiledMap Editor new/Tilesets/Atlas-Props-sheet1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Atlas-Props-sheet1" tilewidth="32" tileheight="32" tilecount="3520" columns="44">
- <image source="../../Props/Static props/Atlas-Props-sheet1.png" width="1408" height="2560"/>
-</tileset>
--- Assets/ERW - Grass Land 2.0 v1.6.1/TiledMap Editor new/Tilesets/Atlas-Props-sheet3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Atlas-Props-sheet3" tilewidth="32" tileheight="32" tilecount="1144" columns="44">
- <image source="../../Props/Static props/Atlas-Props-sheet3.png" width="1408" height="832"/>
-</tileset>
